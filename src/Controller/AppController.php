d8b88d09df0a0baad6243e6b99ee32d097edde4d
Aubriot Marc <marc.aubriot@outlook.fr>
2023-03-14 17:57:26 +0100

    form class

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -2,33 +2,47 @@
 // src/Controller/AppController.php
 namespace App\Controller;
 
+use App\Form\RegisterFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class AppController extends AbstractController
 {
-    #[Route('/', methods: ['GET'])]
-    public function index(): Response
+    #[Route('/')]
+    public function index(Request $request): Response
     {
+        $form = $this->createForm(RegisterFormType::class);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // $form->getData() holds the submitted values
+            // but, the original `$task` variable has also been updated
+            // $task = $form->getData();
+
+            // ... perform some action, such as saving the task to the database
+
+            return $this->redirectToRoute('task_success');
+        }
+
         return $this->render('default/index.html.twig', [
-            
+            'form' => $form,
         ]);
 
-        // ...
+
     }
 
-    #[Route('/register', methods: ['GET'])]
+    #[Route('/register')]
     public function register(): Response
     {
         return $this->render('default/register.html.twig', [
             
         ]);
 
-        // ...
     }
 
-    #[Route('/backoffice', methods: ['GET'])]
+    #[Route('/backoffice')]
     public function backoffice(): Response
     {
         return $this->render('default/backoffice.html.twig', [
---
+++ src/Form/RegisterFormType.php
@@ -0,0 +1,32 @@
+<?php
+// src/Form/RegisterFormType.php
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Validator\Constraints\Email;
+
+class RegisterFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('Email', EmailType::class)
+            ->add('Password', PasswordType::class)
+            ->add('Se_connecter', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
