f312b741bfc0541c7b5e941a0cb46703bc3d3bda
Aubriot Marc <marc.aubriot@outlook.fr>
2023-03-16 20:58:16 +0100

    form input validation

--- src/Form/ConnectionFormType.php
+++ src/Form/ConnectionFormType.php
@@ -5,20 +5,40 @@ namespace App\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\FormType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class ConnectionFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email', EmailType::class, ['label' => 'Adresse Email'])
-            ->add('password', PasswordType::class, ['label' => 'Mot de passe'])
+            ->add('email', EmailType::class, [
+                'label' => 'Adresse Email',
+                'attr' => ['maxlength' => 255],
+                'required' => true,
+                'constraints' => [
+                    new NotBlank(),
+                    new Email(),
+                ],
+            ])
+            ->add('password', PasswordType::class, [
+                'label' => 'Mot de passe',
+                'attr' => ['maxlength' => 255],
+                'required' => true,
+                'constraints' => [
+                    new NotBlank(),
+                    new Length([
+                        'min' => 6,
+                        'max' => 255,
+                        'minMessage' => 'Le mot de passe doit comporter au moins 6 caractères',
+                        'maxMessage' => 'Le mot de passe ne doit pas dépasser 255 caractères',
+                    ])],
+            ])
             ->add('connect', SubmitType::class, ['label' => 'Se connecter'])
         ;
     }
--- src/Form/RegisterFormType.php
+++ src/Form/RegisterFormType.php
@@ -5,21 +5,53 @@ namespace App\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\FormType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class RegisterFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email', EmailType::class, ['label' => 'Adresse Email'])
-            ->add('password', PasswordType::class, ['label' => 'Mot de passe'])
-            ->add('confirmPassword', PasswordType::class, ['label' => 'Confirmation du mot de passe'])
+            ->add('email', EmailType::class, [
+                'label' => 'Adresse Email',
+                'attr' => ['maxlength' => 255],
+                'required' => true,
+                'constraints' => [
+                    new NotBlank(),
+                    new Email(),
+                ],
+            ])
+            ->add('password', PasswordType::class, [
+                'label' => 'Mot de passe',
+                'attr' => ['maxlength' => 255],
+                'required' => true,
+                'constraints' => [
+                    new NotBlank(),
+                    new Length([
+                        'min' => 6,
+                        'max' => 255,
+                        'minMessage' => 'Le mot de passe doit comporter au moins 6 caractères',
+                        'maxMessage' => 'Le mot de passe ne doit pas dépasser 255 caractères',
+                    ])],
+            ])
+            ->add('confirmPassword', PasswordType::class, [
+                'label' => 'Confirmation du mot de passe',
+                'attr' => ['maxlength' => 255],
+                'required' => true,
+                'constraints' => [
+                    new NotBlank(),
+                    new Length([
+                        'min' => 6,
+                        'max' => 255,
+                        'minMessage' => 'Le mot de passe doit comporter au moins 6 caractères',
+                        'maxMessage' => 'Le mot de passe ne doit pas dépasser 255 caractères',
+                    ])],
+            ])
             ->add('connect', SubmitType::class, ['label' => 'Creer mon compte'])
         ;
     }
