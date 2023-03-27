ae988cfd108d698cc2e8c1a739134391ce1900fb
Aubriot Marc <marc.aubriot@outlook.fr>
2023-03-27 10:51:12 +0200

    password checker

--- assets/app.js
+++ assets/app.js
@@ -11,11 +11,61 @@ import './styles/app.css';
 // start the Stimulus application
 import './bootstrap';
 
-let newConsultantBtn = document.getElementById('newConsultantBtnID');
-let newConsultantFormDiv = document.getElementById('newConsultantFormDivID');
-let showNewConsultantFormDiv = false;
-newConsultantBtn.addEventListener('click', ()=> {
-    showNewConsultantFormDiv = !showNewConsultantFormDiv;
-    newConsultantFormDiv.classList = showNewConsultantFormDiv ? "showDiv addConsultantForm" : "hiddenDiv";
-});
+// form > password checker
+var myInput = document.getElementById("register_form_password");
+var letter = document.getElementById("letter");
+var capital = document.getElementById("capital");
+var number = document.getElementById("number");
+var length = document.getElementById("length");
 
+// When the user clicks on the password field, show the message box
+myInput.onfocus = function() {
+  document.getElementById("message").style.display = "block";
+}
+
+// When the user clicks outside of the password field, hide the message box
+myInput.onblur = function() {
+  document.getElementById("message").style.display = "none";
+}
+
+// When the user starts to type something inside the password field
+myInput.onkeyup = function() {
+  // Validate lowercase letters
+  var lowerCaseLetters = /[a-z]/g;
+  if(myInput.value.match(lowerCaseLetters)) {
+    letter.classList.remove("invalid");
+    letter.classList.add("valid");
+  } else {
+    letter.classList.remove("valid");
+    letter.classList.add("invalid");
+}
+
+  // Validate capital letters
+  var upperCaseLetters = /[A-Z]/g;
+  if(myInput.value.match(upperCaseLetters)) {
+    capital.classList.remove("invalid");
+    capital.classList.add("valid");
+  } else {
+    capital.classList.remove("valid");
+    capital.classList.add("invalid");
+  }
+
+  // Validate numbers
+  var numbers = /[0-9]/g;
+  if(myInput.value.match(numbers)) {
+    number.classList.remove("invalid");
+    number.classList.add("valid");
+  } else {
+    number.classList.remove("valid");
+    number.classList.add("invalid");
+  }
+
+  // Validate length
+  if(myInput.value.length >= 8) {
+    length.classList.remove("invalid");
+    length.classList.add("valid");
+  } else {
+    length.classList.remove("valid");
+    length.classList.add("invalid");
+  }
+}
